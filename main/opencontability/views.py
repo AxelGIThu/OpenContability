33f4635dc2debdb1eb264c7f0a06356799b3108a
AxelGIThu <javieraxel375@gmail.com>
2025-04-13 17:00:50 -0300

    Creación Funciones+Base HTML para las 6 funciones principales. Terminé el backend de la función nuevo_cliente.

--- main/opencontability/views.py
+++ main/opencontability/views.py
@@ -2,6 +2,34 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import CrearCliente
 
-
+# Muestra los hipervinculos para las partes de la página.
+# Tendrá verificación de usuarios en un futuro
 def index(request):
     return render(request, "index.html")
+
+# Cargar los clientes.
+def nuevo_clientes(request):
+    if request.method == "POST":
+        # Crea un objeto con los datos del formulario y lo guarda.
+        form = CrearCliente(request.POST)
+        form.save()
+        return HttpResponse("Funciono!<br><a href='nuevo_clientes'>Volver</a>")
+    else:
+        # Crea un objeto con el formulario para el HTML.
+        form = CrearCliente
+    return render(request, "nuevo_clientes.html", { 'form' : form})
+
+def tabla_clientes(request):
+    return render(request, "tabla_clientes.html")
+
+def cargar_facturas(request):
+    return render(request, "cargar_facturas.html")
+
+def tabla_compra_venta(request):
+    return render(request, "tabla_compra_venta.html")
+
+def modificar(request):
+    return render(request, "modificar.html")
+
+def generar_archivos(request):
+    return render(request, "generar_archivos.html")
