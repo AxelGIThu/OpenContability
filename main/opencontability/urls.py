a1ca3a9b585450a0cfebed1a621274992b159007
AxelGIThu <javieraxel375@gmail.com>
2025-04-13 17:00:50 -0300

    Creación Funciones+Base HTML para las 6 funciones principales. Terminé el backend de la función nuevo_cliente.

--- main/opencontability/urls.py
+++ main/opencontability/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("nuevo_clientes", views.nuevo_clientes, name="nuevo_clientes"),
+    path("tabla_clientes", views.tabla_clientes, name="tabla_clientes"),
+    path("cargar_factura", views.cargar_facturas, name="cargar_facturas"),
+    path("tabla_compra_venta", views.tabla_compra_venta, name="tabla_compra_venta"),
+    path("modificar", views.modificar, name="modificar"),
+    path("generar_archivos", views.generar_archivos, name="generar_archivos"),
 ]
