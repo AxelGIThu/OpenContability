a9c30dc27720718aaa6786b958220dd813115d35
AxelGIThu <javieraxel375@gmail.com>
2025-04-27 16:32:51 -0300

    +Fix_urls (Cambie las urls para que tenga mas sentido y sea mas intuitivo), +Agregue el CSS (Hecho por Ivan)

--- main/opencontability/urls.py
+++ main/opencontability/urls.py
@@ -4,11 +4,22 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("nuevo_clientes", views.nuevo_clientes, name="nuevo_clientes"),
-    path("tabla_clientes", views.tabla_clientes, name="tabla_clientes"),
-    path("cargar_facturas", views.cargar_facturas, name="cargar_factura"),
-    path("tabla_compra_venta", views.tabla_compra_venta, name="tabla_compra_venta"),
-    path("modificar_clientes/<int:primary_key>", views.modificar_clientes, name="modificar_clientes"),
-    path("modificar_facturas/<int:primary_key>", views.modificar_facturas, name="modificar_facturas"),
+
+    # Clientes
+    path("clientes", views.index_clientes, name="clientes"),
+    path("clientes/cargar_clientes", views.cargar_clientes, name="cargar_clientes"),
+    path("clientes/tabla_clientes", views.tabla_clientes, name="tabla_clientes"),
+    path("clientes/tabla_clientes/modificar_clientes/<int:primary_key>", views.modificar_clientes, name="modificar_clientes"),
+    ##########
+
+    # Facturas
+    path("facturas", views.index_facturas, name="facturas"),
+    path("facturas/cargar_factura", views.cargar_facturas, name="cargar_facturas"),
+    path("facturas/tabla_facturas", views.tabla_facturas, name="tabla_facturas"),
+    path("facturas/tabla_facturas/modificar_facturas/<int:primary_key>", views.modificar_facturas, name="modificar_facturas"),
+    ###########
+
+    # Generar Archivos
     path("generar_archivos", views.generar_archivos, name="generar_archivos"),
+    ###########
 ]
--- main/opencontability/views.py
+++ main/opencontability/views.py
@@ -9,26 +9,35 @@ from django.db.models import Q
 def index(request):
     return render(request, "index.html")
 
-# Cargar los clientes.
-def nuevo_clientes(request):
+########################################################## Clientes
+def index_clientes(request):
+    return render(request, "clientes.html")
+
+def cargar_clientes(request):
 
     if request.method == "POST":
         # Crea un objeto con los datos del formulario y lo guarda.
         form = CrearCliente(request.POST)
         form.save()
-        return HttpResponse("Funciono!<br><a href='nuevo_clientes'>Volver</a>")
+        return HttpResponse("Funciono!<br><a class='anchor' href='cargar_clientes'>Volver</a>")
     
     else:
         # Crea un objeto con el formulario para el HTML.
         form = CrearCliente
 
-    return render(request, "nuevo_clientes.html", { 'form' : form})
+    return render(request, "cargar_clientes.html", { 'form' : form})
 
 def tabla_clientes(request):
 
     clientes = Clientes.objects.all()
 
     return render(request, "tabla_clientes.html", { 'clientes' : clientes})
+
+##########################################################
+
+########################################################## Facturas
+def index_facturas(request):
+    return render(request, "facturas.html")
 
 def cargar_facturas(request):
 
@@ -39,7 +48,7 @@ def cargar_facturas(request):
             factura = form.save(commit=False)
             factura.calcular_totales()
             factura.save()
-            return HttpResponse("Funciono!<br><a href='cargar_facturas'>Volver</a>")
+            return HttpResponse("Funciono!<br><a class='anchor' href='cargar_facturas'>Volver</a>")
     
     else:
         # Crea un objeto con el formulario para el HTML.
@@ -47,7 +56,7 @@ def cargar_facturas(request):
 
     return render(request, "cargar_facturas.html", { 'form' : form })
 
-def tabla_compra_venta(request):
+def tabla_facturas(request):
 
     if request.method == 'POST':
 
@@ -58,13 +67,16 @@ def tabla_compra_venta(request):
 
             if facturas:
                 # Si hay facturas, las muestra
-                return render(request, "tabla_compra_venta.html", { 'facturas' : facturas , 'columnas' : columnas })
+                return render(request, "tabla_facturas.html", { 'facturas' : facturas , 'columnas' : columnas })
             else:
                 # Si no hay facturas, no las muestra.
-                return render(request, "tabla_compra_venta.html")
+                return render(request, "tabla_facturas.html")
             
-    return render(request, "tabla_compra_venta.html")
+    return render(request, "tabla_facturas.html")
 
+##########################################################
+
+########################################################## Modificar
 def modificar_clientes(request, primary_key):
     cliente = get_object_or_404(Clientes, IDCliente=primary_key)
     form = CrearCliente(instance=cliente)
@@ -77,9 +89,24 @@ def modificar_clientes(request, primary_key):
             return redirect('tabla_clientes')
     return render(request, "modificar_clientes.html", { 'form' : form, 'cliente' : cliente})
 
-def modificar_facturas(request):
+def modificar_facturas(request, primary_key):
+    factura = get_object_or_404(Facturas, NFactura=primary_key)
+    form = CrearFactura(instance=factura)
     
-    return render(request, "modificar_facturas.html")
+    if request.method == 'POST':
 
+        form = CrearFactura(request.POST, instance=factura)
+        if form.is_valid():
+            factura = form.save(commit=False)
+            factura.calcular_totales()
+            form.save()
+            return redirect('tabla_facturas')
+    return render(request, "modificar_facturas.html", { 'form' : form, 'factura' : factura})
+
+##########################################################
+
+########################################################## Generar Archivos
 def generar_archivos(request):
     return render(request, "generar_archivos.html")
+
+##########################################################
