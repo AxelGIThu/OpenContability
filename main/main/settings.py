445f04b6edb45d89c7999214dcca00a32b909c66
AxelGIThu <javieraxel375@gmail.com>
2025-05-09 18:12:19 -0300

    +Modulo archivos (+CSV)

--- main/main/settings.py
+++ main/main/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ocDB',
         'USER': 'postgres',
-        'PASSWORD': '1',
+        'PASSWORD': 'a',
         'HOST': 'localhost',
         'PORT': '5432'
     }
