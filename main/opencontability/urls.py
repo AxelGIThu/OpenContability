9d9e7a728afe7bbd6e48fc2dc1a1043b40e27992
AxelGIThu <javieraxel375@gmail.com>
2025-05-17 17:50:04 -0300

    +sistema_login-logout-register-autenticacion (Falta modificar la base de datos para que funcione en conjunto a los usuarios)

--- main/opencontability/urls.py
+++ main/opencontability/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Usuarios
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('registro/', views.registro, name='registro'),
+    ##########
+
+    # Index
     path("", views.index, name="index"),
+    ##########
 
     # Clientes
     path("clientes", views.index_clientes, name="clientes"),
