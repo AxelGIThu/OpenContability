2288436eca0eda73bc85dbc8fa6df7c39923b814
AxelGIThu <javieraxel375@gmail.com>
2025-06-14 12:57:57 -0300

    +ajustes_render (DEBUG desactivado)

--- main/main/settings.py
+++ main/main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['opencontability.onrender.com']
 
