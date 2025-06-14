d6e26dcc9b294845c276e2f8288578a72ac9098b
AxelGIThu <javieraxel375@gmail.com>
2025-06-14 12:07:07 -0300

    +ajustes_render (arreglo saticfiles_dirs)

--- main/main/settings.py
+++ main/main/settings.py
@@ -134,7 +134,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'opencontability', 'Static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
