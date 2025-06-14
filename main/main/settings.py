78d6129b65bc9540eeff94b8c2c9a89ed6d27f22
AxelGIThu <javieraxel375@gmail.com>
2025-06-14 11:52:16 -0300

    +ajustes_render (allowed_hosts)

--- main/main/settings.py
+++ main/main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('DJANGO_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['opencontability.onrender.com']
 
 
 # Application definition
